0af73f300574936f475884b9e21477989677b246
badjoke-lab <badjoke.lab@gmail.com>
2026-09-24 23:47:39 +0900

    vendor(lane4): import upstream lib/render-config.js

---
+++ vendor/old-school-racing/lib/render-config.js
@@ -0,0 +1,14 @@
+var RenderConfig = function () {
+  this.scale = 0.5;
+  this.width = 1920 * this.scale;
+  this.height = 1080 * this.scale;
+  this.halfWidth = this.width / 2;
+  this.halfHeight = this.height / 2;
+  this.cameraHeight = 35;
+  this.cameraDistance = this.cameraHeight * 1.3;
+  this.fieldOfView = Math.tan(45);
+  this.drawDistance = 5000;
+  this.zoom = 91;
+}
+
+module.exports = RenderConfig;
