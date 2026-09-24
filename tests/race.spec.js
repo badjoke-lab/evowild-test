a134b695e6553b51e945771ac7211ce2bbb9f177
badjoke-lab <badjoke.lab@gmail.com>
2026-09-25 00:11:27 +0900

    test: allow camera capture during longer visual proof

--- tests/race.spec.js
+++ tests/race.spec.js
@@ -44,6 +44,7 @@ test("race scene renders and advances", async ({ page }) => {
 
 
 test("capture mobile race camera views", async ({ page }, testInfo) => {
+  test.setTimeout(45000);
   test.skip(testInfo.project.name !== "android-chromium");
 
   const outDir = "test-results/visuals";
