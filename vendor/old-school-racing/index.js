bd13f93dd5897e3a1d383104295193b0131bf440
badjoke-lab <badjoke.lab@gmail.com>
2026-09-24 23:46:48 +0900

    vendor(lane4): import upstream index.js

---
+++ vendor/old-school-racing/index.js
@@ -0,0 +1,6 @@
+Car = require('./lib/car');
+Track = require('./lib/track');
+Race = require('./lib/race');
+RaceRender = require('./lib/race-render');
+KeyboardControl = require('./lib/keyboard-control');
+BasicCPUControl = require('./lib/basic-cpu-control');
