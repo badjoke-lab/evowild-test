50e9ecb68e0560e56738e2ba12394e5c45373469
badjoke-lab <badjoke.lab@gmail.com>
2026-09-24 23:47:04 +0900

    vendor(lane4): import upstream lib/pseudo3d.js

---
+++ vendor/old-school-racing/lib/pseudo3d.js
@@ -0,0 +1,19 @@
+var Pseudo3D = function (config) {
+  this.config = config;
+}
+
+Pseudo3D.prototype.projectPoint = function(point) {
+  var x = point.x * this.config.scale;
+  var y = (point.y + this.config.cameraHeight) * this.config.scale;
+  var z = (point.z + this.config.cameraDistance) * this.config.scale;
+  var xScale = this.config.width / this.config.fieldOfView;
+  var yScale = this.config.height / this.config.fieldOfView;
+  var scaleCoeficient = this.config.scale * this.config.zoom;
+  return {
+    x: x * xScale / z + this.config.halfWidth,
+    y: y * yScale / z + this.config.halfHeight,
+    s: (1 / (point.z + this.config.cameraDistance)) * scaleCoeficient
+  };
+}
+
+module.exports = Pseudo3D;
