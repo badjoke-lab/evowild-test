4580bca7900e5d1825ae8b861d32084a1bade50c
badjoke-lab <badjoke.lab@gmail.com>
2026-09-24 23:47:44 +0900

    vendor(lane4): import upstream lib/screen-render.js

---
+++ vendor/old-school-racing/lib/screen-render.js
@@ -0,0 +1,37 @@
+var _ = require('underscore');
+var PIXI = require('../engine/pixi');
+
+var ScreenRender = function (config, assets, onLoadComplete, updateCallback) {
+  this.config = config;
+  this.assets = assets;
+  this.updateCallback = updateCallback;
+  this.onLoadComplete = onLoadComplete;
+  this.loader = new PIXI.AssetLoader(this.assets);
+  this.loader.onComplete = this.load.bind(this);
+  this.loader.load();
+}
+
+ScreenRender.prototype.load = function () {
+  this.stage = new PIXI.Stage(0xDDEEFF);
+  this.renderer = new PIXI.autoDetectRenderer(
+    this.config.width, this.config.height
+  );
+  document.body.appendChild(this.renderer.view);
+  if (this.onLoadComplete) this.onLoadComplete();
+  this.render();
+}
+
+ScreenRender.prototype.render = function () {
+  this.updateCallback();
+  this.stage.children = this.getChildrenSortByDepth();
+  this.renderer.render(this.stage);
+  requestAnimFrame(this.render.bind(this));
+}
+
+ScreenRender.prototype.getChildrenSortByDepth = function () {
+  return _(this.stage.children).sortBy(function (child) {
+    return - (child.z || Number.MAX_VALUE);
+  });
+}
+
+module.exports = ScreenRender;
