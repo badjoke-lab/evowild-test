4fdf22f69c3e8e2e83ff98bfe5fa540c9d8f5366
badjoke-lab <badjoke.lab@gmail.com>
2026-09-25 08:50:24 +0900

    test(lane4): serialize WebGL proof captures

--- playwright.lane4.config.js
+++ playwright.lane4.config.js
@@ -12,7 +12,8 @@ const launchOptions = {
 
 export default defineConfig({
   testDir: "./tests",
-  timeout: 120000,
+  timeout: 150000,
+  workers: 1,
   use: {
     baseURL: "http://127.0.0.1:4174",
     trace: "retain-on-failure",
