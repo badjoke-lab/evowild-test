09cd62875735bfa54757650cdf4354f6e72eb021
badjoke-lab <badjoke.lab@gmail.com>
2026-09-24 23:47:02 +0900

    vendor(lane4): import upstream lib/keyboard-control.js

---
+++ vendor/old-school-racing/lib/keyboard-control.js
@@ -0,0 +1,41 @@
+var _ = require('underscore');
+var Keypress = require('../engine/keypress');
+
+var KeyboardControl = function (car) {
+  this.car = car;
+  this.listener = new Keypress.keypress.Listener();
+  this.bindKeys([
+    [
+      'a',
+      function () { this.car.controls.gas = 1 },
+      function () { this.car.controls.gas = 0 }
+    ],
+    [
+      'z',
+      function () { this.car.controls.brake = 1 },
+      function () { this.car.controls.brake = 0 }
+    ],
+    [
+      'right',
+      function () { this.car.controls.steering = -1 },
+      function () { this.car.controls.steering = 0 }
+    ],
+    [
+      'left',
+      function () { this.car.controls.steering = 1 },
+      function () { this.car.controls.steering = 0 }
+    ]
+  ]);
+}
+
+KeyboardControl.prototype.bindKeys = function (keys) {
+  _(keys).each(function (keyMap) {
+    this.listener.register_combo({
+      keys: keyMap[0],
+      on_keydown: keyMap[1].bind(this),
+      on_keyup: keyMap[2].bind(this),
+    });
+  }.bind(this));
+}
+
+module.exports = KeyboardControl;
