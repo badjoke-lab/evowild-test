b2c0bea0620ea0f59a12658fff6014d075d68e6f
badjoke-lab <badjoke.lab@gmail.com>
2026-09-25 00:27:23 +0900

    build: include isolated Road Lab page

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(process.cwd(), "index.html"),
-        race2_5d: resolve(process.cwd(), "race-2_5d.html")
+        race2_5d: resolve(process.cwd(), "race-2_5d.html"),
+        raceRoadlab: resolve(process.cwd(), "race-roadlab.html")
       }
     }
   }
