355c7e235b6047f14f260683b684b984aa9be7a1
badjoke-lab <badjoke.lab@gmail.com>
2026-09-24 23:49:06 +0900

    feat(lane4): expose upstream racer core

---
+++ vendor/old-school-racing/evowild-entry.js
@@ -0,0 +1,7 @@
+const Car = require("./lib/car");
+const Track = require("./lib/track");
+const Race = require("./lib/race");
+const RaceRender = require("./lib/race-render");
+const BasicCPUControl = require("./lib/basic-cpu-control");
+
+window.OldSchoolRacing = { Car, Track, Race, RaceRender, BasicCPUControl };
